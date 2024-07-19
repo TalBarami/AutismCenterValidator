2d033113b68eea399f173fb34132da3b7f50009a
noaBgu <55344859+noaBgu@users.noreply.github.com>
2024-07-19 11:13:33 +0300

    Added executables

---
+++ Validator.bat
@@ -0,0 +1,5 @@
+set PATH=%PATH%;C:\Users\User\anaconda3;C:\Users\User\anaconda3\Scripts
+set PYTHONPATH=%PATH%;E:\AutismCenterValidator;E:\AutismCenterValidator\src
+call activate annotate
+cd "E:/AutismCenterValidator"
+python "src/validator/annotator.py"
